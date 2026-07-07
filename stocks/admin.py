d2520007cfa3441361467a4b46dd92309d085127
SINGULARITY-101 <kwatra.vishesh@gmail.com>
2026-07-07 22:03:03 +0530

    Registered the models

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Stock, WatchlistItem, PortfolioHolding, PriceAlert
 
 # Register your models here.
+
+# admin.site.register() tells Django: "expose this model in the admin UI"
+admin.site.register(Stock)
+admin.site.register(WatchlistItem)
+admin.site.register(PortfolioHolding)
+admin.site.register(PriceAlert)
