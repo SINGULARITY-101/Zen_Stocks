2314dcf5fbf03b1c337206cb36fd37f6b24efa73
SINGULARITY-101 <kwatra.vishesh@gmail.com>
2026-07-08 17:05:04 +0530

    Django auth setup done

---
+++ stocks/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path 
+from . import views    # your app's views.py, where signup_view lives
+
+
+
+"""
+The path() function defines the following:
+    1) A URL Pattern 
+    2) A view function that will be called if the URL pattern is detected
+    3) A name so that you can refer to the URL in your HTML templates
+
+"""
+
+
+
+urlpatterns = [
+    path('signup/', views.signup_view, name='signup'),
+]
